199972ee6b1280710a213152abd7554683239968
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-10-05 12:34:25 +0900

    html/css作成（仮）

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,6 +1,51 @@
+import './App.css';
+
 export const App = () => {
   return (
-    <div className="App">hoge</div>
+    <div className="container">
+      <div className="input-area">
+        <input type="text" placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p>未完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>
+              <p>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </li>
+          </div>
+          <div className="list-row">
+            <li>
+              <p>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </li>
+          </div>
+        </ul>
+      </div>
+      <div className="iomplete-area">
+        <p>完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>
+              <p>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </li>
+          </div>
+          <div className="list-row">
+            <li>
+              <p>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </li>
+          </div>
+        </ul>
+      </div>
+    </div>
   )
 }
 
